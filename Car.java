ee6df7f1b5ecc23a014f53a17999fdd2971797ef
The_Paul_Turner <35648632+ThePaulTurner@users.noreply.github.com>
2021-01-25 23:54:56 -0800

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,25 @@
+class Car {
+    private String maker;
+    private String color;
+    private String bodyType;
+
+    public car(){
+        maker = "Porsche";
+        color = "Silver";
+        bodyType = "Coupe";
+    }
+
+    private String acclerate ()
+    {
+        String motion = "Accelerating ...";
+        return motion;
+
+    }
+
+    public void setCar
+
+
+
+
+
+}
